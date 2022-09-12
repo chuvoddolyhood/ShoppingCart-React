7598f096e980d32ff510f12cc7316fb52d5068a2
Nghia Tran <trannhannghia.24012000@gmail.com>
2022-09-12 23:56:50 +0700

    add list user infor and pick geolocation

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -4,6 +4,7 @@ import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 const User = () => {
     const [user, setUser] = useState([])
     const [loading, setLoading] = useState(true)
+    const [geo, setGeo] = useState({ lat: 10.815867, lng: 106.732017 })
 
     useEffect(() => {
         const getUser = async () => {
@@ -16,26 +17,64 @@ const User = () => {
         getUser()
     }, [])
 
-
     const { isLoaded } = useLoadScript({ googleMapsApiKey: "AIzaSyB6x9uVHtA4LJXl26uyh12h3oOZVqSYpqQ" })
 
     function Map() {
-        const marker = { lat: 10.815867, lng: 106.732017 }
+        const hcmc = { lat: 10.815867, lng: 106.732017 }
+        const cantho = { lat: 10.042895, lng: 105.754108 }
         return (
-            <GoogleMap zoom={2} center={marker} mapContainerStyle={{ width: '100%', height: '100vh' }}>
+            <GoogleMap zoom={4} center={geo} mapContainerStyle={{ width: '100%', height: '50vh' }}>
                 {user.map((geo, index) => {
                     return (
                         <Marker key={index} position={{ lat: Number(geo.address.geolocation.lat), lng: Number(geo.address.geolocation.long) }} />
                     )
                 })}
-                <Marker position={marker} />
+                <Marker position={hcmc} />
+                <Marker position={cantho} />
             </GoogleMap>
+        )
+    }
+
+    const UserInfo = () => {
+        return (
+            <>
+                <h1 style={{ textAlign: 'center' }}>User Information</h1>
+                <table style={{ width: '100%', textAlign: 'center' }}>
+                    <tr>
+                        <th>STT</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        {/* <th>Zipcode</th>
+                        <th>Email</th>
+                        <th>Geolocation</th> */}
+                        <th>View</th>
+                    </tr>
+                    {user.map((us, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{us.id}</td>
+                                <td>{us.name.firstname + ' ' + us.name.lastname}</td>
+                                <td>{us.phone}</td>
+                                <td>{us.address.number + ', ' + us.address.street + ', ' + us.address.city}</td>
+                                {/* <td>{us.address.zipcode}</td>
+                                <td>{us.email}</td>
+                                <td>{us.address.geolocation.lat + ' : ' + us.address.geolocation.long}</td> */}
+                                <button onClick={() => setGeo({ lat: Number(us.address.geolocation.lat), lng: Number(us.address.geolocation.long) })}>View</button>
+                            </tr>
+                        )
+                    })}
+                </table>
+            </>
         )
     }
 
     if (!isLoaded || loading) return <div>Loading...</div>
     return (
-        <Map />
+        <>
+            <Map />
+            <UserInfo />
+        </>
     )
 
 
